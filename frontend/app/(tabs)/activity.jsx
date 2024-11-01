204f30c26c79dedb14b747a8668fe8cb83585212
hmbarnez <hmbarnes007@gmail.com>
2024-11-01 13:18:41 -0400

    added activity submit no redux

--- frontend/app/(tabs)/activity.jsx
+++ frontend/app/(tabs)/activity.jsx
@@ -1,4 +1,6 @@
 import { View, Text, TextInput, TouchableOpacity, ScrollView} from 'react-native';
+import axios from 'axios';
+import moment from 'moment';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import React, { useState } from 'react';
 import ExamActivity from '../../components/activity/ExamActivity';
@@ -9,37 +11,10 @@ import TaskActivity from '../../components/activity/TaskActivity';
 
 const Activity = () => {
   const intitialform = {
-    // Exam fields
-    examSubject: '',
-    examSubjectName: '',
-    examRoom: '',
-    examBuilding: '',
-    examDate: '',
-    examStartTime: '',
-    examEndTime: '',
-    
-    // Class fields
-    classSubject: '',
-    classSubjectName: '',
-    classRoom: '',
-    classBuilding: '',
-    classDays: '', // State to store days for repeating events
-    classStartDate: '',
-    classEndDate: '',
-    classStartTime: '',
-    classEndTime: '',
-
-    // Task fields
-    taskTitle: '',
-    taskDescription: '',
-    taskDate: '',
-    taskStartDate: '',
-    taskEndDate: '',
-    taskStartTime: '',
-    taskEndTime: '',
-    taskDays: '', // State to store days for repeating tasks
-    taskOccurs: 'Once', // Separate state for "Occurs" in Tasks
-  };
+    examSubject: '', examSubjectName: '', examRoom: '', examBuilding: '', examDate: '', examStartTime: '', examEndTime: '',
+    classSubject: '', classSubjectName: '', classRoom: '', classBuilding: '', classDays: '', classStartDate: '', classEndDate: '', classStartTime: '', classEndTime: '',
+    taskTitle: '', taskDescription: '', taskDate: '', taskStartDate: '', taskEndDate: '', taskStartTime: '', taskEndTime: '', taskDays: '', taskOccurs: 'Once'
+};
 
   const [form, setForm] = useState(intitialform);
 
@@ -62,25 +37,62 @@ const Activity = () => {
     return `${hours}:${minutes.toString().padStart(2, '0')} ${period}`;
   };
 
-  //submit form function will put in a separate file
-  // const submitForm = async () => {
-  //   try {
-  //     const docRef = await addDoc(collection(db, 'Eventz'), {
-  //       userId: "testid123",
-  //       examSubject: form.examSubject,
-  //     });
-  //     console.log('Document written with ID: ', docRef.id);
-  //     alert('Success', 'Activity added successfully');
-  //   } catch (e) {
-  //     console.error('Error adding document: ', e);
-  //     alert('Error', 'An error occurred. Please try again later.');
-  //   }
-  // };
+  // get the day of the week using moment and .format('dddd')
+  const getDayOfWeekFromDate = (formDate) => {
+    console.log('formDate:', formDate);
+    const date = moment(formDate, 'YYYY-MM-DD');
+    return date.format('dddd');
+  }
+
 
   const saveActivity = () => {
-    // submitForm();
-    console.log(form);
+    let requestBody = {};
+    if (activityType === 'Exams') {
+      // Create the request body for an exam event
+      requestBody = {
+        subjectName: form.examSubjectName, subject: form.examSubject, building: form.examBuilding, room: form.examRoom,
+        startingDate: form.examDate, endingDate: form.examDate, eventDays: getDayOfWeekFromDate(form.examDate),
+        startingTime: form.examStartTime, endingTime: form.examEndTime, type: 'exam'
+      };
+    } else if (activityType === 'Classes') {
+      // Create the request body for a class event
+      requestBody = {
+        subjectName: form.classSubjectName, subject: form.classSubject, building: form.classBuilding, room: form.classRoom,
+        eventDays: form.classDays, startingDate: form.classStartDate, endingDate: form.classEndDate,
+        startingTime: form.classStartTime, endingTime: form.classEndTime, type: 'class'
+      };
+    } else if (activityType === 'Tasks') {
+      let taskDaysFormatted = form.taskDays, endingDate = form.taskEndDate, startingDate = form.taskStartDate;
+      if (form.taskOccurs === 'Once') {
+          taskDaysFormatted = getDayOfWeekFromDate(form.taskDate);
+          endingDate = form.taskDate;
+          startingDate = form.taskDate;
+      }
+      requestBody = {
+          taskTitle: form.taskTitle, taskDescription: form.taskDescription,
+          eventDays: taskDaysFormatted, startingDate, endingDate,
+          startingTime: form.taskStartTime, endingTime: form.taskEndTime, type: 'task'
+      };
+    } 
+  
+    const userId = 'GwwihkBX4iMUZcWouAku'; // Replace with the actual user ID later with redux
+    // Submit the constructed requestBody  
+    axios.post(`http://localhost:3000/events/${userId}`, requestBody, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      console.log('Event saved successfully:', requestBody);
+      //reset the form
+      alert('Event saved successfully');
+      setForm(intitialform);
+    })
+    .catch(error => {
+      console.error('Error saving event:', error);
+    });
   };
+  
 
   const cancelActivity = () => {
     setForm(intitialform);
