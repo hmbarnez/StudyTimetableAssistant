2df55954cfd257554bf6bd4cd548a5ab2bc43ac0
Muketoi Celestino <muketoicelestino2019@gmail.com>
2024-12-02 14:25:54 -0500

    Changed Feedback to About

--- frontend/app/(settings)/_layout.jsx
+++ frontend/app/(settings)/_layout.jsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function Layout() {
   return (
     <Stack >
-      <Stack.Screen name="feedback" options={{headerShown: false}}/>
+      <Stack.Screen name="about" options={{headerShown: false}}/>
       <Stack.Screen name="notification-settings" options={{headerShown: false}}/>
       <Stack.Screen name="account-settings" options={{headerShown: false}}/>
     </Stack>
